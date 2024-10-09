702bf818c226930c78bd687a84e29094390d26e6
Yadwinder Singh <166858853+YadDhindsa@users.noreply.github.com>
2024-10-09 16:12:28 -0600

    week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>My  Name: Yadwinder Singh </p>
+      <p>
+        GitHub Repository: <Link href=" https://github.com/YadDhindsa/cprg306-assignments.git"> Yadwinder Singh Repository </Link>
+      </p>
+    </div>
+  );
+}
