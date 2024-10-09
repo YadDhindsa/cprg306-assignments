8e09d9e8019c0a5c2a365cee8538f046dfe8466d
Yadwinder Singh <166858853+YadDhindsa@users.noreply.github.com>
2024-10-09 17:27:40 -0600

    weak 3

--- app/page.js
+++ app/page.js
@@ -4,10 +4,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG306 Assignments</h1>
+      <h1> CPRG -306 Assignments</h1>
       <ul>
-        <li><Link href="./week-2/">Week 2 Assignment</Link></li>
-        <li><Link href="./week-3/">Week 3 Assignment</Link></li>
+        <li>
+          <Link href="./week-2/">Week 2 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-3/">Week 3 Assignment</Link>
+        </li>
       </ul>
     </main>
   );
